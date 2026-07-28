f4cc1a8a132ceb08975e87d5cb81f6eb4c9d6ae4
Iosif Ianis-Cosmin <ianis_cosmin.iosif@stud.acs.upb.ro>
2026-07-28 14:35:37 +0300

    new adds

---
+++ help.py
@@ -0,0 +1,68 @@
+
+import special_characters
+
+#TO DO - add all
+arr = special_characters.arrow
+
+# information about commands
+"""
+HELP = {
+    "hello": {},
+    "how are you": {},
+    "info": {},
+    "voices":{},
+    "set voice":{},
+    "exit":{},
+    "try voice": {}
+}
+"""
+
+HELP = {
+    "hello": {
+        "usage": "hello",
+        "description": "Greets the assistant."
+    },
+
+    "how are you": {
+        "usage": "how are you",
+        "description": "Shows the assistant status."
+    },
+
+    "info": {
+        "usage": "info <topic>",
+        "description": "Searches Wikipedia for a topic."
+    },
+
+    "voices": {
+        "usage": "voices",
+        "description": "Displays all available voices."
+    },
+
+    "set voice": {
+        "usage": "set voice <1-5>",
+        "description": "Changes the current voice."
+    },
+
+    "exit": {
+        "usage": "exit",
+        "description": "Terminates the assistant."
+    }
+}
+
+def show_all_commands():
+    print("\nAvailable commands\n")
+    for command, info in HELP.items():
+        print(f"{info['usage']}")
+        print(f"    {info['description']}\n")
+
+def show (command):
+    command = command.lower()
+    if command not in HELP:
+        return "[INFO]: Unknown command"
+    info = HELP[command]
+    return (f"Command: {info['usage']}\n"
+            f"Description: {info['description']}")
+
+def print_in_history():
+    #TO DO
+    pass
---
+++ special_characters.py
@@ -0,0 +1,9 @@
+
+arrow = "\u27A4"
+user_arrow = "\u276F"
+date_icon = "\U0001F4C5"
+time_icon = "\u231A" #clock
+bar = "\u2500"
+bot_icon = "\U0001F916"
+usr_icon = "\U0001F464"
+msg_icon = "\U0001F4AC"
