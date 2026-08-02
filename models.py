d866c251d7434ddb63aa69b4ca1201275eab1b6a
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-02 17:21:37 +0300

    add models.py

---
+++ models.py
@@ -0,0 +1,20 @@
+# TO DO - add in server.py
+
+# Simple model for a user
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+
+    __tablename__ = "users"
+    id = db.Column(db.Integer,primary_key=True)
+
+    username = db.Column(db.String(50), unique=True, nullable=False)
+
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    password_hash = db.Column(db.String(255), nullable=False)
+
+    created_at = db.Column(db.DateTime, nullable=False)
