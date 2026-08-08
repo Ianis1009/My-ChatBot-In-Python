1496ee48faf8a7be05b371fd3e71a242c0383637
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-08 15:23:20 +0300

    geocode_city

--- travel.py
+++ travel.py
@@ -8,8 +8,18 @@ HEADERS = { "User-Agent" : "VoiceAssistant/beta"};
 
 
 def geocode_city(city):
-    #TODO
-    pass
+   params = {"q" : city, "format" : "json", "limit" : 1}
+
+   try:
+       response = requests.get(NOMINATIM_URL, params=params, headers=HEADERS, timeout=10)
+       response.raise_for_status()
+       data = response.json()
+       if not data:
+            return None 
+       return {"name": data[0].get("display_name"), "latitude": float(data[0]["lat"]), "longitude": float(data[0]["lon"])}
+
+   except requests.RequestException:
+    return None 
 
 def calculate_route(origin, festination):
     #TODO
