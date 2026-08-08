96654ed7214a7f47cad4fa0b15c9b0e25543ecf8
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-08 15:20:20 +0300

    add urls and headers in travel.py

--- travel.py
+++ travel.py
@@ -1,5 +1,12 @@
 import requests
 
+
+NOMINATIM_URL = "https://nominatim.openstreetmap.org/search"
+OSRM_URL = "https://router.project-osrm.org/route/v1/driving"
+
+HEADERS = { "User-Agent" : "VoiceAssistant/beta"};
+
+
 def geocode_city(city):
     #TODO
     pass
