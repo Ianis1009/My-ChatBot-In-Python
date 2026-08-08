e5ff152a22df2849849544dfbda49edc3f036abf
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-08 11:15:41 +0300

    models

--- models.py
+++ models.py
@@ -9,6 +9,7 @@ db = SQLAlchemy()
 class User(db.Model):
 
     __tablename__ = "users"
+    
     id = db.Column(db.Integer,primary_key=True)
 
     username = db.Column(db.String(50), unique=True, nullable=False)
