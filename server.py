de349e1a0da350a79f5c590443e9cd5b7f2d4ae3
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-08 11:15:21 +0300

    update server.py

--- server.py
+++ server.py
@@ -31,6 +31,33 @@ def get_voice_state():
 
 @app.route("/chat", methods=["POST"])
 def chat():
+    
+        # data = request.get_json()
+    
+        # if data is None:
+    
+        #     return jsonify({
+        #         "response": "[ERROR]: Invalid request."
+        #     }), 400
+    
+    
+        # command = data.get("message", "").strip()
+    
+        # if command == "":
+        #     return jsonify({
+        #         "response": "[INFO]: Please enter a command."
+        #     }), 400
+    
+        # voice.save_message("User", command)
+        # response = voice.process_command(command)
+        # voice.save_message("Bot", response)
+    
+        # voice.speak(response, voice.current_voice)
+    
+    
+        # return jsonify({
+        #     "response": response
+        # })
 
     data = request.get_json()
     if data is None:
