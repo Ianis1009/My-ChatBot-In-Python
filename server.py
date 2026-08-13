b0d625c76a1d16d6bc6c0803ff3a299bb75a4644
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-13 23:04:58 +0300

    update some bugs

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, send_from_directory
+from flask import Flask, render_template, request, jsonify, send_from_directory, session, flash, redirect, url_for
 
 import os
 
