fb7003df597a28e4a53f236af2823e75f434af7b
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-08-24 23:29:30 +0300

    add let in js

---
+++ static/route3d.js
@@ -0,0 +1,19 @@
+let route3DScene = null;
+let route3DCamera = null;
+let route3DRenderer = null;
+
+let route3DAnimation = null;
+
+let route3DStartPoint = null;
+let route3DEndPoint = null;
+let route3DLine = null;
+
+let route3DDragging = false;
+
+let route3DPreviousMouseX = 0;
+let route3DPreviousMouseY = 0;
+
+let route3DRotationX = 0.45;
+let route3DRotationY = 0.25;
+
+let route3DZoom = 1;
